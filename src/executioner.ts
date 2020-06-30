8830f50b104d5834846b8b94f3a35f99c6c91b88
Curt Elsasser <curt.elsasser@hbo.com>
2020-06-29 23:11:06 -0400

    sort out wonky handling of errors. No more messing around with formatting (#1)
    * - rename webstorm project
    
    * - remove newable stuff from the command interface. Typescript doesn't have proper support for constructor signatures and we don't need it.
    - documentation
    - bump version
    
    * - handle async errors 'cause commander doesn't.
    - honor desired error format
    
    * - revise error handling so that we no longer rely on formatting and can include detail that we want and have it all be less weird.
    
    * - fix lint stuff

--- src/executioner.ts
+++ src/executioner.ts
@@ -5,6 +5,7 @@
  */
 
 import * as commander from "commander";
+import * as _ from "lodash";
 import {CliCommand, CliCommandOptions} from "./types";
 
 /********************
@@ -14,10 +15,13 @@ import {CliCommand, CliCommandOptions} from "./types";
 /**
  * Given the candidates we will process the args and see who is the lucky winner and run him.
  * @param candidates - probably want to use findCliCommands to populate this guy
- * @param args
+ * @param args - may optionally specify your own but `process.args` is most likely going to fit the bill.
  */
-export async function executeCliCommand(candidates: CliCommand[], args: string[] = process.argv): Promise<void> {
+export function executeCliCommand(candidates: CliCommand[], args: string[] = process.argv): Promise<void> {
 	const commander = setupCommander(candidates);
+	// note: we let commander handle synchronous errors and we deal with all command runtime errors. The reason is
+	// that he doesn't handle asynchronous errors and they end up being unhandled. But if we handle them then we
+	// are responsible for logging. And we have special needs for command logging. So we divide responsibility.
 	return commander.parseAsync(args)
 		.then(() => undefined);
 }
@@ -32,14 +36,33 @@ export async function executeCliCommand(candidates: CliCommand[], args: string[]
  * @param args - execution arguments except that commander attaches itself as a final argument. We will remove it.
  */
 async function executeCommand(command: CliCommand, options: commander.Command, ...args: any[]): Promise<void> {
-	return command.execute(options as CliCommandOptions, ...args.slice(0, args.length-1));
+	return command.execute(options as CliCommandOptions, ...args.slice(0, args.length-1))
+		.catch(error => {
+			// For the reasons documented above in `executeCliCommand` we handle async errors here and snuff the error.
+			// But don't really care one way or the other 'cause we are exiting the process.
+			if("debug" in options) {
+				console.error(error);
+			} else {
+				console.error(`error: ${error.message}`);
+			}
+			process.exit(_.get(error, "statusCode", 1));
+		});
 }
 
+/**
+ * Loads package.json and pulls out the version
+ */
 function getModuleVersion(): string {
 	const packageJson = require("../package.json");
 	return packageJson.version;
 }
 
+/**
+ * Creates and configures an instance of `Command`:
+ * - setups up boiler plate options
+ * - configures our `candidates`
+ * - sets up execution handlers
+ */
 function setupCommander(candidates: CliCommand[]): commander.Command {
 	const program = new commander.Command()
 		.version(getModuleVersion());
